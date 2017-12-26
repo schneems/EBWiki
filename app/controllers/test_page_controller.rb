bd6a5168e26eee06dfc082d5226ebebbd03bf5e0
Darius Goore <dariusgoore@gmail.com>
2017-12-26 05:24:34 -0500

    removing testpage controller

--- app/controllers/test_page_controller.rb
+++
@@ -1,5 +0,0 @@
-class TestPageController < ApplicationController
-	def show
-		render template: "test_page/#{params[:page]}"
-	end
-end
--- app/controllers/test_pages_controller.rb
+++
@@ -1,5 +0,0 @@
-class TestPagesController < ApplicationController
-  def show
-  	render template: "test_pages/#{params[:page]}"
-  end
-end
